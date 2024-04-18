6102e0819fe00f09051985fbf7dbcd1e240212c2
aditya-0212 <adityachoubisa02@gmail.com>
2024-04-18 06:45:16 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by APPLE on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
